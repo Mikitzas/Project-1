8e9d82e22d083a1c66cbf984aff9ac3c809bdb94
milos <milos.bulat002@gmail.com>
2024-02-24 16:26:52 +0100

    rename files name

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
-
 const colorChange = document.querySelectorAll(".colorchange");
 
 colorChange.forEach((button) => {
   button.addEventListener("mouseover", (e) => {
     let bgColor = window.getComputedStyle(e.currentTarget).getPropertyValue("filter");
-    console.log(bgColor);
-
-    console.log(this)
-
     let nextSibling = e.currentTarget.nextElementSibling
     let imgElement = nextSibling.querySelector("img");
     let hidenImg = nextSibling.querySelector("img:nth-child(2)")
     hidenImg.style.display = 'block';
     hidenImg.style.filter = bgColor
-    console.log(nextSibling);
-    console.log(imgElement);
-    console.log(hidenImg)
+  }); 
 
-  });
-  
   button.addEventListener("mouseout", (e) => {
     let nextSibling = e.currentTarget.nextElementSibling;
     let hiddenImg = nextSibling.querySelector("img:nth-child(2)");
